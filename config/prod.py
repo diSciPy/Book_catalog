ae1beef8a7d0f6209eb38a264c4203a4331dd7d4
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 21:31:43 +0300

    trhoubleshoot 1.9

--- config/prod.py
+++ config/prod.py
@@ -8,6 +8,6 @@ SECRET_KEY = os.getenv( 'SECRET_KEY' )
 uri = os.getenv('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv( 'SQLALCHEMY_TRACK_MODIFICATIONS', False )
 LANGUAGES = ['en', 'uk_UA']
