c9dc9f7153b1ade61aa6bc249e5efd6b1ab62368
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 21:48:39 +0300

    trch

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app, db
 
+
+#if __name__=="__main__":
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
@@ -7,6 +9,6 @@ with flask_app.app_context():
     #     User.create_user(user='harry',
     #                      email='harry@potter.com',
     #                      password='secret')
-flask_app.run()
+    flask_app.run()
 
 ##gunicorn - http server web will be run
