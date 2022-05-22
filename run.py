6411fe1c5531eb6da53b4f7c400586b671797bbe
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 20:27:59 +0300

    edit run.py for eWSGI

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 from app import create_app, db
+from app import app as application
 from app.authentication.models import User, UserVerification
 
-flask_app = create_app('prod')
-with flask_app.app_context():
+application = create_app('prod')
+with application.app_context():
     db.create_all()
     if not User.query.filter_by(user_name='harry').first():
         User.create_user(user='harry',
                          email='harry@potter.com',
                          password='secret')
-    flask_app.run()
+    application.run()
 
 ##gunicorn - http server web will be run
