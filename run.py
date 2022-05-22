cba9fc5c202febae52a9183d73474c3ffa9609f2
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 22:00:52 +0300

    troubleshooting 1.12

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 from app import create_app, db
 
 
-if __name__=="__main__":
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        # if not User.query.filter_by(user_name='harry').first():
-        #     User.create_user(user='harry',
-        #                      email='harry@potter.com',
-        #                      password='secret')
-        #flask_app.run()
+#if __name__=="__main__":
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    # if not User.query.filter_by(user_name='harry').first():
+    #     User.create_user(user='harry',
+    #                      email='harry@potter.com',
+    #                      password='secret')
+    #flask_app.run()
 
 ##gunicorn - http server web will be run
