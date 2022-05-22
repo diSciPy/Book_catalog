a00bf374603f925a0c96be208a53af40a4a0c4a2
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 19:58:14 +0300

    troubleshooting 1.4

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from app.authentication.models import User, UserVerification
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    # if not User.query.filter_by(user_name='harry').first():
-    #     User.create_user(user='harry',
-    #                      email='harry@potter.com',
-    #                      password='secret')
-flask_app.run()
+    if not User.query.filter_by(user_name='harry').first():
+        User.create_user(user='harry',
+                         email='harry@potter.com',
+                         password='secret')
+        flask_app.run()
 
 ##gunicorn - http server web will be run
