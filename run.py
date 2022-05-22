c97aec732dc407b658c91908c0c8a2ff91724c56
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 20:39:00 +0300

    troubleshooting 1.7

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 from app import create_app, db
 from app import app as application
-from app.authentication.models import User, UserVerification
 
-application = create_app('prod')
-with application.app_context():
-    db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry',
-                         email='harry@potter.com',
-                         password='secret')
+if __name__ == '__main__':
+
+    application = create_app('prod')
+    with application.app_context():
+        db.create_all()
+        # if not User.query.filter_by(user_name='harry').first():
+        #     User.create_user(user='harry',
+        #                      email='harry@potter.com',
+        #                      password='secret')
     application.run()
 
 ##gunicorn - http server web will be run
