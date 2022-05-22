c237147cb95c67d8ca6a06429ad1c2dc4dc78f5e
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 20:47:45 +0300

    edit requirements.txt

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 from app import create_app, db
 from app import app as application
 
-if __name__ == '__main__':
 
-    application = create_app('prod')
-    with application.app_context():
-        db.create_all()
-        # if not User.query.filter_by(user_name='harry').first():
-        #     User.create_user(user='harry',
-        #                      email='harry@potter.com',
-        #                      password='secret')
-    application.run()
+application = create_app('prod')
+with application.app_context():
+    db.create_all()
+    # if not User.query.filter_by(user_name='harry').first():
+    #     User.create_user(user='harry',
+    #                      email='harry@potter.com',
+    #                      password='secret')
+application.run()
 
 ##gunicorn - http server web will be run
