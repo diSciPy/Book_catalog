ee9ab823e0651b3d98f336c7d60a5cf5d7dc614d
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 21:03:22 +0300

    switch to gunicorn from uwsgi

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
 from app import create_app, db
-from app import app as application
 
-
-application = create_app('prod')
+flask_app = create_app('prod')
 with application.app_context():
     db.create_all()
     # if not User.query.filter_by(user_name='harry').first():
     #     User.create_user(user='harry',
     #                      email='harry@potter.com',
     #                      password='secret')
-application.run()
+flask_app.run()
 
 ##gunicorn - http server web will be run
