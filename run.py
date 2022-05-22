4ec8755828105c0b837e8dc2c0dc877cbf631216
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 18:33:37 +0300

    prepare to launch on heroku

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from app.authentication.models import User, UserVerification
 
 if __name__=='__main__':
-    flask_app = create_app('dev')
+    flask_app = create_app('prod')
     with flask_app.app_context():
         db.create_all()
         # if not User.query.filter_by(user_name='harry').first():
