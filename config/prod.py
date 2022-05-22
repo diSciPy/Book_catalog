c8d844afdba5586b15d9d5f994afe2c15077916d
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-22 21:28:42 +0300

    edit db connection env var to comply with Heroku

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,13 @@
 import os
+import re
 
 DEBUG = os.getenv( 'DEBUG', False )
 SECRET_KEY = os.getenv( 'SECRET_KEY' )
+
+#to comply with heroku connection to postgre SQL DB
+uri = os.getenv('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv( 'SQLALCHEMY_TRACK_MODIFICATIONS', False )
 LANGUAGES = ['en', 'uk_UA']
