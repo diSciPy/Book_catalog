18acc6d7a4231bf70a087376155a59aa764facef
Diana Babyna <diana.babyna.a@gmail.com>
2022-05-25 17:17:37 +0300

    change env vars

--- config/prod.py
+++ config/prod.py
@@ -4,9 +4,10 @@ import re
 DEBUG = os.getenv( 'DEBUG', False )
 
 #to comply with heroku connection to postgre SQL DB
-uri = os.getenv('DATABASE_URL')
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv( 'SQLALCHEMY_TRACK_MODIFICATIONS', False )
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 LANGUAGES = ['en', 'uk_UA']
+BABEL_TRANSLATION_DIRECTORIES = os.getenv('BABEL_TRANSLATION_DIRECTORIES')
